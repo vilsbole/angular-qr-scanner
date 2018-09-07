472bf02fd1603bfa401008ed3ff342d19263d31f
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2018-09-07 11:38:10 +0200

    use webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  output: {
+    filename: 'angular-qr-scanner.min.js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+  ],
+  externals: [
+    'canvas',
+    'angular',
+    'jsqrcode'
+  ],
+  devtool: 'source-map',
+};
